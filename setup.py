e5bd7a7bea396bc805128f7a7fb979fb8540c802
miniPCB <111667004+miniPCB@users.noreply.github.com>
2025-04-22 21:35:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gitgpt",
-    version="0.1.0",
+    version="0.1.5",
     description="AI-powered Git commit assistant using GPT and git diff",
     author="Nolan Manteufel",
     packages=find_packages(),
